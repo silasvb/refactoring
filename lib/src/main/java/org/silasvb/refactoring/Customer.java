48cc4e3c40c6244a522c9491b251abcd3287dabf
Silas Benson <silas.benson@landregistry.gov.uk>
2024-02-16 23:08:31 +0000

    Basic Scenario

---
+++ lib/src/main/java/org/silasvb/refactoring/Customer.java
@@ -0,0 +1,71 @@
+package org.silasvb.refactoring;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+class Customer {
+
+  private String name;
+  private List<Rental> rentals;
+
+  public Customer(String name) {
+    this.name = name;
+    rentals = new ArrayList<>();
+  }
+
+  public void addRental(Rental rental) {
+    rentals.add(rental);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String statement() {
+    double totalAmount = 0;
+    int frequentRenterPoints = 0;
+
+    String result = "Rental Record for " + getName() + "\n";
+
+    Enumeration _rentals = Collections.enumeration(rentals);
+    while (_rentals.hasMoreElements()) {
+      double thisAmount = 0;
+      Rental each = (Rental) _rentals.nextElement();
+
+      switch (each.getMovie().getPiceCode()) {
+        case Movie.REGULAR:
+          thisAmount += 2;
+          if (each.getDaysRented() > 2) {
+            thisAmount += (each.getDaysRented() - 2) * 1.5;
+          }
+          break;
+        case Movie.NEW_RELEASE:
+          thisAmount += each.getDaysRented() * 3;
+          break;
+
+        case Movie.CHILDRENS:
+          thisAmount += 1.5;
+          if (each.getDaysRented() > 3) {
+            thisAmount += (each.getDaysRented() - 3) * 1.5;
+          }
+          break;
+      }
+
+      frequentRenterPoints++;
+
+      if (each.getMovie().getPiceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 2) {
+        frequentRenterPoints++;
+      }
+
+      result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+      totalAmount += thisAmount;
+    }
+
+    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+    result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+
+    return result;
+  }
+}
---
+++ lib/src/main/java/org/silasvb/refactoring/Rental.java
@@ -0,0 +1,19 @@
+package org.silasvb.refactoring;
+
+class Rental {
+  private Movie movie;
+  private int daysRented;
+
+  public Rental(Movie movie, int daysRented) {
+    this.movie = movie;
+    this.daysRented = daysRented;
+  }
+
+  public int getDaysRented() {
+    return daysRented;
+  }
+
+  public Movie getMovie() {
+    return movie;
+  }
+}
