1c231d6143847db7c73a8dc1ba98589197425e29
Silas Benson <silas.benson@landregistry.gov.uk>
2024-02-17 07:41:09 +0000

    Extract Method

--- lib/src/main/java/org/silasvb/refactoring/Customer.java
+++ lib/src/main/java/org/silasvb/refactoring/Customer.java
@@ -31,28 +31,9 @@ class Customer {
 
     Enumeration _rentals = Collections.enumeration(rentals);
     while (_rentals.hasMoreElements()) {
-      double thisAmount = 0;
       Rental each = (Rental) _rentals.nextElement();
 
-      switch (each.getMovie().getPiceCode()) {
-        case Movie.REGULAR:
-          thisAmount += 2;
-          if (each.getDaysRented() > 2) {
-            thisAmount += (each.getDaysRented() - 2) * 1.5;
-          }
-          break;
-        case Movie.NEW_RELEASE:
-          thisAmount += each.getDaysRented() * 3;
-          break;
-
-        case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (each.getDaysRented() > 3) {
-            thisAmount += (each.getDaysRented() - 3) * 1.5;
-          }
-          break;
-      }
-
+      double thisAmount = getAmountForRental(each);
       frequentRenterPoints++;
 
       if (each.getMovie().getPiceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1) {
@@ -67,5 +48,28 @@ class Customer {
     result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 
     return result;
+  }
+
+  private double getAmountForRental(Rental each) {
+    double thisAmount = 0;
+    switch (each.getMovie().getPiceCode()) {
+      case Movie.REGULAR:
+        thisAmount += 2;
+        if (each.getDaysRented() > 2) {
+          thisAmount += (each.getDaysRented() - 2) * 1.5;
+        }
+        break;
+      case Movie.NEW_RELEASE:
+        thisAmount += each.getDaysRented() * 3;
+        break;
+
+      case Movie.CHILDRENS:
+        thisAmount += 1.5;
+        if (each.getDaysRented() > 3) {
+          thisAmount += (each.getDaysRented() - 3) * 1.5;
+        }
+        break;
+    }
+    return thisAmount;
   }
 }
