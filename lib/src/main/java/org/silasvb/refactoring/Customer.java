ffffc72a1b4e0d86be550462ee3f42fccb280337
Silas Benson <silas.benson@landregistry.gov.uk>
2024-02-17 10:42:41 +0000

    Extract Frequent Rental Points calculation

--- lib/src/main/java/org/silasvb/refactoring/Customer.java
+++ lib/src/main/java/org/silasvb/refactoring/Customer.java
@@ -24,27 +24,27 @@ class Customer {
   }
 
   public String statement() {
-    int frequentRenterPoints = 0;
 
     String result = "Rental Record for " + getName() + "\n";
 
     Enumeration _rentals = Collections.enumeration(rentals);
     while (_rentals.hasMoreElements()) {
       Rental each = (Rental) _rentals.nextElement();
-
-      frequentRenterPoints += each.getFrequentRenterPoints();
-
       result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
     }
 
     result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-    result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+    result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
 
     return result;
   }
 
   private double getTotalCharge() {
     return rentals.stream().mapToDouble(Rental::getCharge).sum();
+  }
+
+  private int getTotalFrequentRenterPoints() {
+    return  rentals.stream().mapToInt(Rental::getFrequentRenterPoints).sum();
   }
 
 }
